b11a01f10149954dee8b36b25b44ffaf4ac6e1ec
Harvey Reynier <56921263+hreynier@users.noreply.github.com>
2025-06-22 15:51:11 +0100

    Add string formatter for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,17 @@
 package config
 
+import (
+	"fmt"
+)
+
 type Config struct {
 	Currency   string   `yaml:"currency"`
 	DateFormat string   `yaml:"date_format"`
 	Categories []string `yaml:"categories"`
 	UserName   string   `yaml:"user_name"`
 	DataFile   string   `yaml:"data_file"`
+}
+
+func (cfg Config) String() string {
+	return fmt.Sprintf("Name: %s, Currency: %s, Date Format: %s, Categories: %v, Data File: %s", cfg.UserName, cfg.Currency, cfg.DateFormat, cfg.Categories, cfg.DataFile)
 }
